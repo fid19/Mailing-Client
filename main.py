9911bc66f3c24c5c8321192bebc2b43c8800e989
flashc0de <femiomoj2001@gmail.com>
2022-04-03 06:52:13 -0400

    Mailing Client Done

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-import socket
+import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
+server = smtplib.SMTP_SSL('smtp.gmail.com:465')
+
+server.ehlo()
+
+
+with open('password.txt', 'r') as f:
+    password = f.read()
+
+server.login("flashc0de404@gmail.com", "***************")
+
+msg = MIMEMultipart()
+msg['From'] = 'NeuralNine'
+msg['To'] = 'signaturesignat@gmail.com'
+msg['Subject'] = 'Just a Text'
+
+with open ('message.txt', 'r') as f:
+    message = f.read()
+
+msg.attach(MIMEText(message, 'plain'))
+
+filename = 'img.png'
+attachment = open(filename, 'rb')
+
+p = MIMEBase('application', 'octet-stream')
+p.set_payload(attachment.read())
+
+encoders.encode_base64(p)
+p.add_header('Context-Disposition', f'attachment; filename={filename}')
+msg.attach(p)
+
+text = msg.as_string()
+
+server.sendmail('flashc0de404@gmail.com', 'signaturesignat@gmail.com', text)
+
